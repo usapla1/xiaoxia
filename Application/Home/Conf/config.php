b7c8170ce976f26d1c8a1789a41b599b7bf1a356
usapla1 <usapla@outlook.com>
2016-03-31 20:05:04 +0800

    修改隐私数据

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -19,13 +19,13 @@ return array(
     'DB_PREFIX'                 => 'cb_',
     'DB_DEBUG'                  =>  TRUE, // 数据库调试模式 开启后可以记录SQL日志 3.2.3新增
 
-    // 配置邮件发送服务器
+    // 配置邮件发送服务器 建议163邮箱
     'MAIL_HOST' =>'smtp.163.com',//smtp服务器的名称
     'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证
-    'MAIL_USERNAME' =>'18958063477@163.com',//你的邮箱名
-    'MAIL_FROM' =>'18958063477@163.com',//发件人地址
+    'MAIL_USERNAME' =>'',//你的邮箱名
+    'MAIL_FROM' =>'',//发件人地址
     'MAIL_FROMNAME'=>'李玉鑫',//发件人姓名
-    'MAIL_PASSWORD' =>'c506901303',//邮箱密码
+    'MAIL_PASSWORD' =>'',//邮箱密码
     'MAIL_CHARSET' =>'utf-8',//设置邮件编码
     'MAIL_ISHTML' =>TRUE, // 是否HTML格式邮件
 
