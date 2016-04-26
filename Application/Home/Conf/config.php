7bd93c4405770b363d5a3d689d0b2834b409cbcb
usapla1 <usapla@outlook.com>
2016-04-26 15:17:07 +0800

    最终版本4

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -22,10 +22,10 @@ return array(
     // 配置邮件发送服务器 建议163邮箱
     'MAIL_HOST' =>'smtp.163.com',//smtp服务器的名称
     'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证
-    'MAIL_USERNAME' =>'m18958063477@163.com',//你的邮箱名
-    'MAIL_FROM' =>'m18958063477@163.com',//发件人地址
-    'MAIL_FROMNAME'=>'李玉鑫',//发件人姓名
-    'MAIL_PASSWORD' =>'c506901303',//邮箱密码
+    'MAIL_USERNAME' =>'',//你的邮箱名
+    'MAIL_FROM' =>'',//发件人地址
+    'MAIL_FROMNAME'=>'',//发件人姓名
+    'MAIL_PASSWORD' =>'',//邮箱密码
     'MAIL_CHARSET' =>'utf-8',//设置邮件编码
     'MAIL_ISHTML' =>TRUE, // 是否HTML格式邮件
 
